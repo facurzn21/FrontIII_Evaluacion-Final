42d934dd080c1b125b71b134196e0aa7013ef3f8
facurzn21 <facundoeze@gmail.com>
2023-09-13 13:16:30 -0300

    Fix  imagen en card
    Imagen en card de dentista

--- evaluacion-final/src/components/Card.jsx
+++ evaluacion-final/src/components/Card.jsx
@@ -1,21 +1,22 @@
 import React, { useContext } from 'react';
 import { Card, CardBody, CardTitle, CardSubtitle, Button, Row, Col, Container } from 'reactstrap';
 import { UserContext } from '../contexts/UserContext';
-
+import doctorImage from '../../public/images/doctor.jpg';
 
 const MyCard = ({ user }) => (
   <Card style={{ margin: "15px 0", minHeight: "250px", maxHeight: "250px" }}>
+    <div style={{ height: '100px'}}>
+      <img src={doctorImage} alt="Doctor" style={{ width: '60%', height: 'auto' }} />
+    </div>
     <CardBody>
-      <CardTitle tag="h5">{user.name}</CardTitle>
-      <CardSubtitle tag="h6" className="mb-2 text-muted">{user.username}</CardSubtitle>
-      <Button>Button</Button>
+      <CardTitle tag="h6">{user.name}</CardTitle>
+      <CardSubtitle tag="h7" className="mb-2 text-muted">{user.username}</CardSubtitle>
     </CardBody>
   </Card>
 );
 
 const Cards = (props) => {
   const users = useContext(UserContext);
-  
   return (
     <Container>
       <Row>
@@ -28,5 +29,4 @@ const Cards = (props) => {
     </Container>
   );
 };
-
 export default Cards;
