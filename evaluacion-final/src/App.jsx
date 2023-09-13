dc2e527d26279f98d58c72dca543d0e26ccf49be
facurzn21 <facundoeze@gmail.com>
2023-09-13 10:51:07 -0300

    fix componente Card
    Se corrige componente card y se customiza para luego poner los dentistas

--- evaluacion-final/src/App.jsx
+++ evaluacion-final/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import Example from './components/Card'
+import Cards from './components/Card'
+
+
 
 
 function App() {
@@ -8,7 +10,7 @@ function App() {
 
   return (
     <>
-      <Example/>
+      <Cards/>
     </>
   )
 }
--- evaluacion-final/src/components/Card.jsx
+++ evaluacion-final/src/components/Card.jsx
@@ -1,118 +1,59 @@
 import React from 'react';
 import { Card, CardText, CardBody, CardTitle, CardSubtitle, Button, Row, Col, Container } from 'reactstrap';
 
-const Example = (props) => {
+const MyCard = ({ title, subtitle, text }) => (
+  <Card>
+    <CardBody>
+      <CardTitle tag="h5">{title}</CardTitle>
+      <CardSubtitle tag="h6" className="mb-2 text-muted">{subtitle}</CardSubtitle>
+      <CardText>{text}</CardText>
+      <Button>Button</Button>
+    </CardBody>
+  </Card>
+)
 
+const Cards = (props) => {
   return (
     <Container>
       <Row>
         <Col sm="3">
-          <Card>
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some text.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
+          <MyCard title="Card title" subtitle="Card subtitle" text="Some text." />
         </Col>
         <Col sm="3">
-          <Card>
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some text.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
+          <MyCard title="Card title" subtitle="Card subtitle" text="Some text." />
         </Col>
         <Col sm="3">
-          <Card>
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some text.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
+          <MyCard title="Card title" subtitle="Card subtitle" text="Some text." />
         </Col>
         <Col sm="3">
-          <Card>
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some text.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
+          <MyCard title="Card title" subtitle="Card subtitle" text="Some text." />
         </Col>
       </Row>
       <Row>
         <Col sm="3">
-          <Card>
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some text.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
+          <MyCard title="Card title" subtitle="Card subtitle" text="Some text." />
         </Col>
         <Col sm="3">
-          <Card>
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some text.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
+          <MyCard title="Card title" subtitle="Card subtitle" text="Some text." />
         </Col>
         <Col sm="3">
-          <Card>
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some text.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
+          <MyCard title="Card title" subtitle="Card subtitle" text="Some text." />
         </Col>
         <Col sm="3">
-          <Card>
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some text.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
+          <MyCard title="Card title" subtitle="Card subtitle" text="Some text." />
         </Col>
       </Row>
       <Row>
-        <Col sm="6">
-          <Card>
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some text.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
+        <Col sm="3">
+          <MyCard title="Card title" subtitle="Card subtitle" text="Some text." />
         </Col>
-        <Col sm="6">
-          <Card>
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some text.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
+        <Col sm="3">
+          <MyCard title="Card title" subtitle="Card subtitle" text="Some text." />
         </Col>
       </Row>
+
     </Container>
   );
 };
 
-export default Example;
+export default Cards;
