8be9a81546753cf2132f1f758308aa6373275bcc
facurzn21 <facundoeze@gmail.com>
2023-09-14 12:20:19 -0300

    Funcionalidad de ifnformacion de detalle
    Detalle para cada Card

--- evaluacion-final/src/components/Card.jsx
+++ evaluacion-final/src/components/Card.jsx
@@ -1,19 +1,23 @@
 import React, { useContext } from 'react';
-import { Card, CardBody, CardTitle, CardSubtitle, Button, Row, Col, Container } from 'reactstrap';
+import { useNavigate } from 'react-router-dom';
+import { Card, CardBody, CardTitle, CardSubtitle, Button, Row, Col, Container, } from 'reactstrap';
 import { UserContext } from '../contexts/UserContext';
 import doctorImage from '../../public/images/doctor.jpg';
 
-const MyCard = ({ user }) => (
-  <Card style={{ margin: "15px 0", minHeight: "250px", maxHeight: "250px" }}>
-    <div style={{ height: '100px'}}>
-      <img src={doctorImage} alt="Doctor" style={{ width: '60%', height: 'auto' }} />
-    </div>
-    <CardBody>
-      <CardTitle tag="h6">{user.name}</CardTitle>
-      <CardSubtitle tag="h7" className="mb-2 text-muted">{user.username}</CardSubtitle>
-    </CardBody>
-  </Card>
-);
+const MyCard = ({ user }) => {
+  const navigate = useNavigate();
+  return (
+    <Card style={{ margin: "15px 0", minHeight: "250px", maxHeight: "250px" }} onClick={() => navigate(`/details/${user.id}`)}>
+      <div style={{ height: '100px' }}>
+        <img src={doctorImage} alt="Doctor" style={{ width: '60%', height: 'auto' }} />
+      </div>
+      <CardBody>
+        <CardTitle tag="h6">{user.name}</CardTitle>
+        <CardSubtitle tag="h7" className="mb-2 text-muted">{user.username}</CardSubtitle>
+      </CardBody>
+    </Card>
+  )
+};
 
 const Cards = (props) => {
   const users = useContext(UserContext);
@@ -29,4 +33,5 @@ const Cards = (props) => {
     </Container>
   );
 };
+
 export default Cards;
--- evaluacion-final/src/routes/Detalle.jsx
+++ evaluacion-final/src/routes/Detalle.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Detail = () => {
- 
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
 
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  }, [id]);
+
   return (
-    <>
-      <h1>Detail Dentist id </h1>
-      {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
-      {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
-    </>
-  )
-}
+    <div>
+      { user && 
+        <div>
+          <h2>{user.name}</h2>
+          <p>{user.phone}</p>
+          <a href={`http://${user.website}`}>{user.website}</a>
+          <p>{user.email}</p>
+        </div>
+      }
+    </div>
+  );
+};
 
 export default Detail
