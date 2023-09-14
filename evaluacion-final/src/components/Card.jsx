9d4e213a778a8fa22d93b9ea17821579290671bd
facurzn21 <facundoeze@gmail.com>
2023-09-14 12:40:40 -0300

    Estilos para Card
    Se crea Card.css y se pasan los estilos para el archivo css

--- evaluacion-final/src/components/Card.jsx
+++ evaluacion-final/src/components/Card.jsx
@@ -3,13 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import { Card, CardBody, CardTitle, CardSubtitle, Button, Row, Col, Container, } from 'reactstrap';
 import { UserContext } from '../contexts/UserContext';
 import doctorImage from '../../public/images/doctor.jpg';
+import './Card.css';
 
 const MyCard = ({ user }) => {
   const navigate = useNavigate();
   return (
-    <Card style={{ margin: "15px 0", minHeight: "250px", maxHeight: "250px" }} onClick={() => navigate(`/details/${user.id}`)}>
-      <div style={{ height: '100px' }}>
-        <img src={doctorImage} alt="Doctor" style={{ width: '60%', height: 'auto' }} />
+    <Card className="myCard" onClick={() => navigate(`/details/${user.id}`)}>
+      <div className="cardImage">
+        <img className="cardImg" src={doctorImage} alt="Doctor" />
       </div>
       <CardBody>
         <CardTitle tag="h6">{user.name}</CardTitle>
